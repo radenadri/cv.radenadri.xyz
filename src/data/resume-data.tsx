a78695af9f13030e795efb72a75e49eff3fe8c59
Adriana Eka Prayudha <radenadriep@gmail.com>
2024-06-15 18:03:20 +0700

    fix: updating resume data

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -6,10 +6,11 @@ export const RESUME_DATA = {
   location: "Bandung, Indonesia, GMT+7",
   locationLink: "https://maps.app.goo.gl/sV57Te2gtvwFDWsx9",
   about:
-    "Enthusiastic developer that specialized in web development with a passion for creating engaging digital experiences",
+    "Enthusiastic mobile developer with a passion for creating engaging digital experiences",
   summary:
-    "Enthusiastic Web Developer. Love to creating a usable websites and mobile applications. Lately i was working with PHP and Laravel with the main focus on the backend, when i needed to work with frontend, i bring my Vue.js and also React.js knowledge to tackling it. Also working with Flutter currently as a Flutter Developer.",
-  avatarUrl: "https://lh3.googleusercontent.com/d/1QijJiJCKnASrp6_wAex_HeJeVkfBDDT8",
+    "Love to creating a usable mobile applications. Currently working as a Flutter Developer.",
+  avatarUrl:
+    "https://lh3.googleusercontent.com/d/1QijJiJCKnASrp6_wAex_HeJeVkfBDDT8",
   personalWebsiteUrl: "https://radenadri.xyz",
   contact: {
     email: "radenadriep@gmail.com",
@@ -22,9 +23,9 @@ export const RESUME_DATA = {
       },
       {
         name: "LinkedIn",
-        url: "https://www.linkedin.com/in/radenadri/",
+        url: "https://www.linkedin.com/in/radenadri",
         icon: LinkedInIcon,
-      }
+      },
     ],
   },
   education: [
@@ -40,48 +41,28 @@ export const RESUME_DATA = {
       company: "Pentacode",
       link: "https://pentacode.id",
       badges: ["Hybrid"],
-      title: "Flutter Developer",
+      title: "Mobile Developer",
       // logo: ParabolLogo,
-      start: "2023",
+      start: "2022",
       end: "now",
       description:
         "Maintaining existing apps and creating new features, also creating new apps. Technologies: Dart, Flutter, Firebase.",
     },
-    {
-      company: "Pentacode",
-      link: "https://pentacode.id",
-      badges: ["Onsite"],
-      title: "Web Developer",
-      // logo: ParabolLogo,
-      start: "2019",
-      end: "2023",
-      description:
-        "Implemented new features, worked on improving the website performance, and maintained the website. Technologies: Laravel, Vue / React and Tailwind.",
-    },
   ],
   skills: [
-    "Tailwind",
-    "PHP",
     "Laravel",
-    "Alpine.js",
-    "Livewire",
-    "JavaScript",
-    "TypeScript",
-    "React.js",
     "Vue.js",
+    "Node.js",
     "Dart",
     "Flutter",
     "Firebase",
-    "Supabase",
+    "SQLite",
+    "PostgreSQL",
   ],
   projects: [
     {
       title: "Friends Entertainment",
-      techStack: [
-        "PHP",
-        "Laravel",
-        "Bootstrap",
-      ],
+      techStack: ["PHP", "Laravel", "Bootstrap"],
       description: "A company profile website for Friends Entertainment.",
       // logo: ConsultlyLogo,
       link: {
@@ -91,13 +72,9 @@ export const RESUME_DATA = {
     },
     {
       title: "MMHC Care",
-      techStack: [
-        "Side Project",
-        "PHP",
-        "Wordpress",
-        "Elementor",
-      ],
-      description: "A company profile website for wellness clinic named MMHC Care.",
+      techStack: ["Side Project", "PHP", "Wordpress", "Elementor"],
+      description:
+        "A company profile website for wellness clinic named MMHC Care.",
       // logo: ConsultlyLogo,
       link: {
         label: "mmhc-care.id",
@@ -105,52 +82,8 @@ export const RESUME_DATA = {
       },
     },
     {
-      title: "Wadzpay API Sandboxing",
-      techStack: [
-        "PHP",
-        "Laravel",
-        "Vue",
-      ],
-      description: "This site purpose is to be the documentation of how to integrate with WadzPay wallet API",
-      // logo: ConsultlyLogo,
-      link: {
-        label: "integration.wadzpay.id",
-        href: "https://integration.wadzpay.com",
-      },
-    },
-    {
-      title: "Dishub Live CCTV",
-      techStack: [
-        "PHP",
-        "Laravel",
-        "Vue",
-      ],
-      description: "DISHUB CCTV Monitoring is an online platform that provides monitoring services for CCTV cameras in the several sections of provincial roads in West Java, Indonesia.",
-      // logo: ConsultlyLogo,
-      link: {
-        label: "cctv.jabarprov.go.id",
-        href: "https://cctv.jabarprov.go.id",
-      },
-    },
-    {
-      title: "Webreg BPOM",
-      techStack: [
-        "PHP",
-        "Laravel",
-      ],
-      description: "Webreg BPOM is a web application that provides an information for registered commodity in Indonesia including foods, medicine and others.",
-      // logo: ConsultlyLogo,
-      link: {
-        label: "cekbpom.pom.go.id",
-        href: "https://cekbpom.pom.go.id/new",
-      },
-    },
-    {
       title: "Simtaru PUPR Kota Sungai Penuh",
-      techStack: [
-        "PHP",
-        "Laravel",
-      ],
+      techStack: ["PHP", "Laravel"],
       description: "Landing page for Simtaru PUPR Kota Sungai Penuh",
       // logo: ConsultlyLogo,
       link: {
@@ -160,11 +93,9 @@ export const RESUME_DATA = {
     },
     {
       title: "Jelajah Family Club",
-      techStack: [
-        "Dart",
-        "Flutter",
-      ],
-      description: "The mobile apps for Jelajah Family Club, the apps can be downloaded in store. User can register to the apps and attending the class that currently available",
+      techStack: ["Dart", "Flutter"],
+      description:
+        "The mobile apps for Jelajah Family Club, the apps can be downloaded in store. User can register to the apps and attending the class that currently available",
       // logo: ConsultlyLogo,
       link: {
         label: "Play Store / App Store",
@@ -173,10 +104,7 @@ export const RESUME_DATA = {
     },
     {
       title: "Passport",
-      techStack: [
-        "Dart",
-        "Flutter",
-      ],
+      techStack: ["Dart", "Flutter"],
       description: "Mobile HR apps. The solution for modern HR.",
       // logo: ConsultlyLogo,
       link: {
@@ -186,28 +114,13 @@ export const RESUME_DATA = {
     },
     {
       title: "Sound of Justice Radio",
-      techStack: [
-        "Dart",
-        "Flutter",
-      ],
-      description: "Sound of Justice is an apps that used by Kemenkumham for their official radio station program. User can listen the broadcast through this app.",
+      techStack: ["Dart", "Flutter"],
+      description:
+        "Sound of Justice is an apps that used by Kemenkumham for their official radio station program. User can listen the broadcast through this app.",
       // logo: ConsultlyLogo,
       link: {
         label: "Play Store / App Store",
         href: "https://play.google.com/store/apps/details?id=id.pentacode.sound_of_justice",
-      },
-    },
-    {
-      title: "SIPENGUIN TARU",
-      techStack: [
-        "Dart",
-        "Flutter",
-      ],
-      description: "SIPENGUIN TARU are apps that created for the residents of Kota Sungai Penuh. The apps purpose is to submit an application for an area use permit within the city area.",
-      // logo: ConsultlyLogo,
-      link: {
-        label: "Play Store / App Store",
-        href: "https://play.google.com/store/apps/details?id=id.pentacode.gis_sungai_penuh",
       },
     },
   ],
