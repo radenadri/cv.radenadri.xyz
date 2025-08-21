12c8455dbd348519623869fa09ccdd5b14022ad2
Adriana Eka Prayudha <radenadriep@gmail.com>
2025-08-22 00:34:13 +0700

    fix: update to 2025

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -5,10 +5,12 @@ export const RESUME_DATA = {
   initials: "AEP",
   location: "Bandung, Indonesia, GMT+7",
   locationLink: "https://maps.app.goo.gl/sV57Te2gtvwFDWsx9",
-  about:
-    "Enthusiastic developer with a passion for creating engaging digital experiences",
-  summary:
-    "Love to creating a usable web and mobile applications. Currently working as a Full Stack Developer.",
+  about: `Enthusiastic developer with a passion for creating engaging digital, love to creating a usable web
+applications to solve the business problems.`,
+  summary: `Skilled frontend developer with focus on developing and
+maintaining front-end web applications. Led the implementation of new front-end development system which
+resulted in website performance improvement and user satisfaction. Adept in collaborating with cross-
+functional teams and delivering projects on-time and on-budget.`,
   avatarUrl:
     "https://res.cloudinary.com/dvoj90vcy/image/upload/v1727768323/closeup_ogukcv.jpg",
   personalWebsiteUrl: "https://radenadri.xyz",
@@ -29,68 +31,103 @@ export const RESUME_DATA = {
       company: "Pentacode",
       link: "https://pentacode.id",
       badges: ["Hybrid"],
-      title: "Full Stack Developer",
+      title: "Frontend Developer",
       // logo: ParabolLogo,
       start: "2022",
-      end: "now",
-      description:
-        "Maintaining existing and also creating new web and mobile apps and creating new features. Technologies: PHP, Laravel, Node.js, Dart, Flutter.",
+      end: "2025",
+      description: `Collaborating with designers and product managers to translate UI/UX designs into functional apps. Writing clean, maintainable, and efficient code.
+        Implementing responsive designs to ensure seamless user experiences across various devices. Built a UI for inventory management, e-commerce, HR apps, payment gateway dashboard and custom tailored apps by request, mostly using React`,
+    },
+    {
+      company: "Self-employed",
+      link: "",
+      badges: ["Freelance"],
+      title: "Freelance Web Developer",
+      // logo: ParabolLogo,
+      start: "2020",
+      end: "2022",
+      description: `
+      Creating website with Wordpress and Laravel. Improving existing web apps with latest technology. Making database schema. Improving applications performance by tuning the query.
+      `,
     },
   ],
   skills: [
-    "Dart",
-    "Flutter",
-    "Android",
-    "Javascript",
-    "Node.js",
+    "HTML",
+    "CSS",
+    "JavaScript",
+    "TypeScript",
+    "React.js",
+    "Vue.js",
     "PHP",
     "Laravel",
     "Wordpress",
+    "MySQL",
+    "MariaDB",
     "SQLite",
+    "Redis",
+    "Flutter",
     "Firebase",
-    "PostgreSQL",
+    "Sentry",
+    "Dart",
+    "Flutter",
+    "Android",
   ],
   projects: [
+    {
+      title: "Biofarma Mediverse RME",
+      techStack: ["React", "Redux", "TypeScript", "Tailwind CSS"],
+      description:
+        "INTERNAL USE ONLY - ERP system for Biofarma Mediverse RME. Backed with Frappe Framework and Python. Frontend with React and Redux.",
+      link: {
+        label: "#",
+        href: "#",
+      },
+    },
+    {
+      title: "POS Properti Payment Gateway",
+      techStack: ["PHP", "Laravel", "PostgreSQL", "NodeJS", "TypeScript"],
+      description:
+        "INTERNAL USE ONLY - A payment gateway portal for POS Properti.",
+      link: {
+        label: "#",
+        href: "#",
+      },
+    },
+    {
+      title: "Smart DP10 - Delivery Platform",
+      techStack: ["React", "React Query", "TypeScript"],
+      description: `INTERNAL USE ONLY - Building the admin panel for handle the service delivery to user MSISDN.`,
+      link: {
+        label: "#",
+        href: "#",
+      },
+    },
+    {
+      title: "StarBP",
+      techStack: ["Dart", "Flutter", "Android"],
+      description:
+        "INTERNAL USE ONLY - A mobile app for StarBP sales intelligence.",
+      link: {
+        label: "PlayStore",
+        href: "https://play.google.com/store/apps/details?id=id.pentacode.bintang&hl=id",
+      },
+    },
+    {
+      title: "Face Recognition service with DeepFace",
+      techStack: ["Python", "DeepFace", "FastAPI", "MySQL", "Docker"],
+      description: "A face recognition service for attendance system.",
+      link: {
+        label: "github.com/radenadri/face-recognition",
+        href: "https://github.com/radenadri/face-recognition",
+      },
+    },
     {
       title: "Friends Entertainment",
       techStack: ["PHP", "Laravel", "Bootstrap"],
       description: "A company profile website for Friends Entertainment.",
-      // logo: ConsultlyLogo,
       link: {
         label: "friends.co.id",
         href: "https://friends.co.id",
-      },
-    },
-    {
-      title: "MMHC Care",
-      techStack: ["Side Project", "PHP", "Wordpress", "Elementor"],
-      description:
-        "A company profile website for wellness clinic named MMHC Care.",
-      // logo: ConsultlyLogo,
-      link: {
-        label: "mmhc-care.id",
-        href: "https://mmhc-care.id",
-      },
-    },
-    {
-      title: "Jelajah Family Club",
-      techStack: ["Dart", "Flutter"],
-      description:
-        "The mobile apps for Jelajah Family Club, the apps can be downloaded in store. User can register to the apps and attending the class that currently available",
-      // logo: ConsultlyLogo,
-      link: {
-        label: "Play Store / App Store",
-        href: "https://play.google.com/store/apps/details?id=id.pentacode.jelajah_family_club",
-      },
-    },
-    {
-      title: "Passport",
-      techStack: ["Dart", "Flutter"],
-      description: "Mobile HR apps. The solution for modern HR.",
-      // logo: ConsultlyLogo,
-      link: {
-        label: "Play Store / App Store",
-        href: "https://play.google.com/store/apps/details?id=id.pentacode.portal",
       },
     },
   ],
