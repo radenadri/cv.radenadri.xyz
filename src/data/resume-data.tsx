8c74f2267d9eb84120547c43b1b38cc359865ff8
Adriana Eka Prayudha <radenadriep@gmail.com>
2025-10-06 15:59:11 +0700

    feat: updating resume

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -1,9 +1,9 @@
-import { GitHubIcon, LinkedInIcon, XIcon } from "@/components/icons";
+import { GitHubIcon, LinkedInIcon } from "@/components/icons";
 
 export const RESUME_DATA = {
   name: "Adriana Eka Prayudha",
   initials: "AEP",
-  location: "Bandung, Indonesia, GMT+7",
+  location: "Jakarta, Indonesia, GMT+7",
   locationLink: "https://maps.app.goo.gl/sV57Te2gtvwFDWsx9",
   about: `Enthusiastic developer with a passion for creating engaging digital, love to creating a usable web
 applications to solve the business problems.`,
@@ -22,6 +22,11 @@ functional teams and delivering projects on-time and on-budget.`,
         name: "GitHub",
         url: "https://github.com/radenadri",
         icon: GitHubIcon,
+      },
+      {
+        name: "LinkedIn",
+        url: "https://linkedin.com/in/radenadri",
+        icon: LinkedInIcon,
       },
     ],
   },
@@ -44,7 +49,7 @@ functional teams and delivering projects on-time and on-budget.`,
       badges: ["Freelance"],
       title: "Freelance Web Developer",
       // logo: ParabolLogo,
-      start: "2020",
+      start: "2019",
       end: "2022",
       description: `
       Creating website with Wordpress and Laravel. Improving existing web apps with latest technology. Making database schema. Improving applications performance by tuning the query.
@@ -52,82 +57,52 @@ functional teams and delivering projects on-time and on-budget.`,
     },
   ],
   skills: [
-    "HTML",
-    "CSS",
     "JavaScript",
     "TypeScript",
-    "React.js",
-    "Vue.js",
+    "React",
+    "React Router",
+    "Next.js",
     "PHP",
     "Laravel",
-    "Wordpress",
-    "MySQL",
-    "MariaDB",
+    "PostgreSQL",
     "SQLite",
     "Redis",
     "Flutter",
     "Firebase",
     "Sentry",
-    "Dart",
-    "Flutter",
-    "Android",
+    "Python",
+    "Django",
+    "FastAPI",
   ],
   projects: [
-    {
-      title: "Biofarma Mediverse RME",
-      techStack: ["React", "Redux", "TypeScript", "Tailwind CSS"],
-      description:
-        "INTERNAL USE ONLY - ERP system for Biofarma Mediverse RME. Backed with Frappe Framework and Python. Frontend with React and Redux.",
-      link: {
-        label: "#",
-        href: "#",
-      },
-    },
     {
       title: "POS Properti Payment Gateway",
       techStack: ["PHP", "Laravel", "PostgreSQL", "NodeJS", "TypeScript"],
       description:
         "INTERNAL USE ONLY - A payment gateway portal for POS Properti.",
       link: {
-        label: "#",
-        href: "#",
+        label: "View study case",
+        href: "https://radenadri.xyz/work/pos-properti-payment-gateway",
       },
     },
     {
-      title: "Smart DP10 - Delivery Platform",
-      techStack: ["React", "React Query", "TypeScript"],
-      description: `INTERNAL USE ONLY - Building the admin panel for handle the service delivery to user MSISDN.`,
-      link: {
-        label: "#",
-        href: "#",
-      },
-    },
-    {
-      title: "StarBP",
-      techStack: ["Dart", "Flutter", "Android"],
+      title: "Passport",
+      techStack: ["Dart", "Flutter", "Android", "iOS"],
       description:
-        "INTERNAL USE ONLY - A mobile app for StarBP sales intelligence.",
+        "INTERNAL USE ONLY - Mobile app for attendance solution for employees and HR teams. Applied in my previous company, Pentacode. ",
       link: {
-        label: "PlayStore",
-        href: "https://play.google.com/store/apps/details?id=id.pentacode.bintang&hl=id",
+        label: "View study case",
+        href: "https://radenadri.xyz/work/passport",
       },
     },
     {
       title: "Face Recognition service with DeepFace",
       techStack: ["Python", "DeepFace", "FastAPI", "MySQL", "Docker"],
-      description: "A face recognition service for attendance system.",
+      description:
+        "A face recognition service for attendance system. Applied in my previous company for integrations with Passport app.",
       link: {
-        label: "github.com/radenadri/face-recognition",
-        href: "https://github.com/radenadri/face-recognition",
-      },
-    },
-    {
-      title: "Friends Entertainment",
-      techStack: ["PHP", "Laravel", "Bootstrap"],
-      description: "A company profile website for Friends Entertainment.",
-      link: {
-        label: "friends.co.id",
-        href: "https://friends.co.id",
+        label: "View source code",
+        href: "https://github.com/radenadri/simple-frs",
       },
     },
   ],
