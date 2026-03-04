baddadecf32c49fbbcaa8ef0f25e54d37b809b31
Adriana Eka Prayudha <radenadriep@gmail.com>
2026-03-04 12:47:12 +0700

    feat: modify works

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -3,10 +3,10 @@ import { GitHubIcon, LinkedInIcon } from "@/components/icons";
 export const RESUME_DATA = {
   name: "Adriana Eka Prayudha",
   initials: "AEP",
-  location: "Jakarta, Indonesia, GMT+7",
+  location: "Bandung, Indonesia, GMT+7",
   locationLink: "https://maps.app.goo.gl/sV57Te2gtvwFDWsx9",
-  about: `Enthusiastic developer with a passion for creating engaging digital, love to creating a usable web and mobile applications to solve the business problems.`,
-  summary: `Skilled fullstack engineer with focus on developing and maintaining web and mobile applications. Led the implementation of new system which resulted in website performance improvement and user satisfaction.`,
+  about: `Building modern web and mobile applications with a focus on functionality and aesthetics.`,
+  summary: `Skilled fullstack developer with focus on developing and maintaining web and mobile applications. Led the implementation of new system which resulted in website performance improvement and user satisfaction.`,
   avatarUrl:
     "https://res.cloudinary.com/dvoj90vcy/image/upload/v1727768323/closeup_ogukcv.jpg",
   personalWebsiteUrl: "https://radenadri.xyz",
@@ -32,23 +32,23 @@ export const RESUME_DATA = {
       company: "Self-employed",
       link: "",
       badges: ["Freelance"],
-      title: "Freelance Fullstack Engineer",
+      title: "Freelance Fullstack Developer",
       // logo: ParabolLogo,
-      start: "2025",
+      start: "Oct 2025",
       end: "now",
       description: `
-      Develop websites and applications with responsive designs and enhance features using the latest technologies. Design and optimize database schemas, including query tuning, to improve application performance. Implement full-stack solutions using Laravel and React for application development needs.
+      Develop solutions and helping people reach their goals in technology needs. Design and optimize database schemas, including query tuning, to improve application performance. Implement full-stack solutions using Laravel and React for web applications and Flutter / React Native for mobile apps.
       `,
     },
     {
       company: "Pentacode",
       link: "https://pentacode.id",
       badges: ["Hybrid"],
-      title: "Fullstack PHP Developer",
+      title: "Fullstack Developer",
       // logo: ParabolLogo,
-      start: "2022",
-      end: "2025",
-      description: `Build and develop responsive web interfaces using Wordpress, Laravel and React including features for e-commerce, HR, inventory, and payment dashboards. Build a backend and also build a web services (REST API) to be integrated with the frontend, also collaborate with backend developers for API integration, feature enhancements, and ensuring performance and cross-device compatibility. Writing code documentation, building application schema diagram and also ERD or any application flow that needed. Write clean, structured, and maintainable code using modern practices. Use Git for version control, debugging, and ensuring an optimal user experience through responsive design and frontend optimization.`,
+      start: "Aug 2022",
+      end: "Sep 2025",
+      description: `Build and develop web apps using Laravel and React including features for e-commerce, HR, inventory, and payment dashboards. Build and develop cross platform mobile application (Android & iOS) using Flutter and React Native. Write clean, structured, and maintainable code using modern practices. Use Git for version control and debugging.`,
     },
   ],
   skills: [
@@ -63,30 +63,42 @@ export const RESUME_DATA = {
     "Laravel",
     "WordPress",
     "Git",
-    "MySQL",
     "PostgreSQL",
     "Redis",
+    "Dart",
+    "Flutter",
+    "Firebase",
   ],
   projects: [
     {
-      title: "POS Properti Payment Gateway",
-      techStack: ["PHP", "Laravel", "PostgreSQL", "NodeJS", "TypeScript"],
-      description:
-        "INTERNAL USE ONLY - A payment gateway portal for POS Properti.",
-      link: {
-        label: "View study case",
-        href: "https://radenadri.xyz/work/pos-properti-payment-gateway",
-      },
+      title: "AITI Coding.bn Mentor",
+      techStack: ["HTML", "CSS", "Javascript", "React"],
+      description: `Coding.bn is a learning programme to train youth jobseekers with the necessary in-demand programming skills so they are more employable and industry-ready. It also aims to tackle the issue on unemployment in Brunei Darussalam. I had the opportunity to join and participate as a Frontend Developer mentor in this program. The material I focus on is basic web programming from HTML, CSS, Javascript and React.js`,
+    },
+    {
+      title: "PT Pos Properti Indonesia",
+      techStack: ["Typescript", "Node.js", "Express"],
+      description: `I work on several internal projects from Pos Properti Indonesia, including creating a web service for payment integration with local banks in Indonesia, used in internal POS applications in several business lines, namely PointArena and PointLab. Then I also helped develop a web app for e-signing documents. The technology I used for developing the services is Node.js with Express.`,
+    },
+    {
+      title: "Sound of Justice",
+      techStack: ["Dart", "Flutter"],
+      description: `Sound of Justice Radio is a live radio application that hosted by Kejaksaan Tinggi RI to be one of their program to be the channel for education and also entertainment. Built with Flutter because we needed a cross-platform framework. I also taking parts in publishing the apps in Play Store and App Store.`,
     },
     {
       title: "Passport",
-      techStack: ["Dart", "Flutter", "Android", "iOS"],
-      description:
-        "INTERNAL USE ONLY - Mobile app for attendance solution for employees and HR teams. Applied in my previous company, Pentacode. ",
-      link: {
-        label: "View study case",
-        href: "https://radenadri.xyz/work/passport",
-      },
+      techStack: ["Dart", "Flutter"],
+      description: `My team and I received input from management to develop our own HRIS System because there are still many manual recap operations and attendance systems that have not yet been assessed as data integrated. Our team finally developed an HRIS application which is used for daily operations in our office including daily attendance, timeoff requests, business trips, and others. I took the initiative to fill the empty mobile developer position to develop a mobile app using Flutter because we needed a cross-platform framework. I also taking parts in publishing the apps in Play Store and App Store.`,
+    },
+    {
+      title: "CekBPOM",
+      techStack: ["PHP", "Laravel", "PostgreSQL"],
+      description: `Cek BPOM is a website created by BPOM as a place to check the validity and registration numbers of products circulating on the Indonesian market. I successfully migrated the codebase to Laravel 11 and also migrated the database from SQL Server to PostgreSQL.`,
+    },
+    {
+      title: "PT. KAI Indonesia",
+      techStack: ["PHP", "Laravel", "PostgreSQL"],
+      description: `My first challenging project which gave me quite a pleasant experience, I was given an assignment to develop an initial version of a web-based application in the form of displaying train arrival and departure schedules posted at train stations under KAI. Currently it is installed on many shows on stations under DAOP 1 and DAOP 2. The initial version was built with PHP and Laravel with PostgreSQL database.`,
     },
   ],
 } as const;
